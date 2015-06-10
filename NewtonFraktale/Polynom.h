f238c216f6fa6cde3c8ab88b314f3abec8393bb0
Joachim Meyer <joachim@war-of-universe.com>
2015-06-10 22:20:08 +0200

    Finally adding Polynom files

---
+++ NewtonFraktale/Polynom.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <vector>
+#include <CL\opencl.h>
+#include <complex>
+#include <PolyString.h>
+
+using namespace Polycode;
+using namespace std;
+
+class Polynom {
+public:
+	Polynom();
+	~Polynom();
+
+	void addCoefficient(complex<cl_double> nC);
+	void differentiate();
+
+	complex<cl_double> getValue(complex<cl_double> nC);
+
+	int getNumCoefficients();
+	complex<cl_double> getCoefficient(int idx);
+
+	String printPolynom();
+
+	//ToDo: implement
+	static Polynom* readFromString(String polynom);
+
+	struct cl_complex* getCLCoefficients();
+private:
+	std::vector<complex<cl_double>> coefficients;
+};
+
